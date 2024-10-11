6c308383b2290883c1f77566da96c46af6941d3d
Damir <damiratamuratov07@gmail.com>
2024-10-11 16:13:54 +0500

    default config

---
+++ src/app/second.ts
@@ -0,0 +1,3 @@
+export const second = () => {
+	console.log('2nd')
+}
---
+++ webpack.config.ts
@@ -0,0 +1,80 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path from 'path'
+import webpack from 'webpack'
+import type { Configuration as DevServerConfig } from 'webpack-dev-server'
+
+enum Mode {
+	Dev = 'development',
+	Prod = 'production',
+}
+
+interface EnvVars {
+	readonly mode: Mode
+	readonly port: number
+}
+
+const devServerConfig = (
+	isDev: boolean,
+	port: number = 5000
+): DevServerConfig | undefined => {
+	if (!isDev) return
+	return {
+		port,
+		open: true,
+	}
+}
+
+export default (env: EnvVars) => {
+	const isDev = env.mode === Mode.Dev
+
+	const config: webpack.Configuration = {
+		mode: env.mode ?? Mode.Dev,
+		entry: path.resolve(__dirname, 'src', 'app', 'main.ts'),
+		output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: '[name].[contenthash].js',
+			clean: true,
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: path.resolve(__dirname, 'public', 'index.html'),
+			}),
+		],
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: {
+						loader: 'swc-loader',
+						options: {
+							jsc: {
+								parser: {
+									syntax: 'typescript',
+									tsx: true,
+								},
+								target: 'es5',
+								loose: false,
+								minify: {
+									compress: false,
+									mangle: false,
+								},
+							},
+							module: {
+								type: 'es6',
+							},
+							minify: false,
+							isModule: true,
+						},
+					},
+				},
+			],
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js'],
+		},
+		devtool: isDev && 'inline-source-map',
+		devServer: devServerConfig(isDev, env.port),
+	}
+
+	return config
+}
